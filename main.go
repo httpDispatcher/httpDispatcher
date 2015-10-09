3201c2905c7b635f6526de4794363af8bdc85243
WangChunsheng <chunshengster@gmail.com>
2015-10-09 17:50:19 +0800

    Add main.go file changed

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
+// param: lower case
+// Struct unit: Upper Case
+// Func: golang style
+
 import (
+	"fmt"
+	"os"
 	//	"flag"
 	//	"fmt"
 	//	"io"
 	//	"log"
 	//	"os"
+	"log/syslog"
 	"query"
 )
 
 func main() {
-	//	logger := log.New(os.Stderr, "_httpDispather_", log.Lshortfile)
-	qdc := new(query.DomainConfig)
-	qdc.AuthoritativeServers = append(qdc.AuthoritativeServers, "8.8.8.8")
+	loger, e := syslog.New(syslog.LOG_DEBUG, "httpDispacher")
+	if e != nil {
+		fmt.Println("Syslog Instance is error, exiting....")
+		os.Exit(1)
+	}
+	loger.Debug("Starting httpDispacher...")
+
+	qdc := new(query.Domain)
+	qdc.AuthoritativeServers = append(qdc.AuthoritativeServers, "ns1.a.shifen.com.")
 	qdc.Port = query.NS_SERVER_PORT
-	query.Test("www.google.com", "106.186.24.134", qdc)
+	//	query.Test("www.a.shifen.com.", "106.186.24.134", qdc)
+
+	qdc.AuthoritativeServers = nil
+	qdc.AuthoritativeServers = append(qdc.AuthoritativeServers, "ns1.baidu.com")
+	//	qdc.SetAS("ns2.baidu.com")
+	ns, e := query.QueryNS("www.baidu.com", qdc)
+	fmt.Println(ns)
 
 }
