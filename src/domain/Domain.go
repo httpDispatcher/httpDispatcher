29fbbe88881928830492a1726efcb56ed5a8d504
WangChunsheng <chunshengster@gmail.com>
2015-12-05 10:21:33 +0800

    Refact Domain.go
    1, replace yasushi-saito/rbtree with GoLLRB/llrb
     2,refact struct Domain
     3,add MuLLRB struct for both LLRB and Mutex
     4,add init function for this module
     5,add Less func for llrb lib
     6,add some interface for DomainTree and RegionTree (both of them are alias of MuLLRB)

--- src/domain/Domain.go
+++ src/domain/Domain.go
@@ -2,20 +2,41 @@ package domain
 
 import (
 	"MyError"
-	"errors"
-	"github.com/miekg/dns"
-	"github.com/yasushi-saito/rbtree"
-	"net"
-	"strconv"
-	"strings"
+	"fmt"
+	"os"
 	"sync"
+	"time"
+
+	"github.com/miekg/dns"
+	"github.com/petar/GoLLRB/llrb"
 )
 
+type MuLLRB struct {
+	llrb.LLRB
+	sync.Mutex
+}
+
+//TODO: redundant data types, need to be redesign
 type Domain struct {
-	DomainName   string
-	Ns           []string
-	Ttl          uint32
-	RegionRRTree *rbtree.Tree
+	DomainName string
+	NS         []*dns.NS
+	TTL        uint32
+}
+
+type DomainNode struct {
+	Domain
+	RegionRRTree *MuLLRB
+}
+
+//TODO: redundant data types, need to be redesign
+// dns.RR && RrType && TTL
+type Region struct {
+	IpStart    uint32
+	IpEnd      uint32
+	RR         []*dns.RR
+	RrType     dns.Type
+	TTL        uint32
+	UpdateTime time.Time
 }
 
 type DomainConfig struct {
@@ -25,35 +46,97 @@ type DomainConfig struct {
 	Ttl                  string
 }
 
+type DomainTree MuLLRB
+type RegionTree MuLLRB
+
 var once sync.Once
+var DomainDB *DomainTree
 
-// type DomainTree rbtree.Tree
-
-var DomainDB *rbtree.Tree
-
-func NewDomain(d string, ns []string, t uint32) (*Domain, error) {
-	if ns == nil {
-		ns = []string{""}
-	}
-
-	if (len(d) > 0) && (len(ns) > 0) {
-		return &Domain{DomainName: d,
-			Ns:  ns,
-			Ttl: t,
-		}, nil
+func init() {
+	db := InitDomainDB()
+	if db == nil {
+		fmt.Println("InitDomainDB failed!")
+		os.Exit(2)
 	} else {
-		return nil, errors.New("param error ! " + d + ":" + strings.Join(ns, ",") + ":" + strconv.Itoa(int(t)))
+		fmt.Println("InitDomainDB succ!")
 	}
 }
 
-func InitDomainDB() *rbtree.Tree {
-	once.Do(func() {
-		DomainDB = rbtree.NewTree(func(a, b rbtree.Item) int {
-			return strings.Compare(a.(*Domain).DomainName, b.(*Domain).DomainName)
-		})
+func (a *DomainNode) Less(b llrb.Item) bool {
+	if x, ok := b.(*DomainNode); ok {
+		return a.DomainName < x.DomainName
+	} else if y, ok := b.(*Domain); ok {
+		return a.DomainName < y.DomainName
+	}
+	return false
+}
+func (a *Domain) Less(b llrb.Item) bool {
+	if x, ok := b.(*DomainNode); ok {
+		return a.DomainName < x.DomainName
+	} else if y, ok := b.(*Domain); ok {
+		return a.DomainName < y.DomainName
+	}
+	return false
+}
 
+func InitDomainDB() *MuLLRB {
+	once.Do(func() {
+		DomainDB = &MuLLRB{}
+		return DomainDB
 	})
-	return DomainDB
+	return nil
+}
+
+func (DT *DomainTree) StoreDomain(d *Domain) {
+
+}
+
+func (DT *DomainTree) SearchDomain(d *Domain) {
+
+}
+
+func (DT *DomainTree) UpdateDomain(d *Domain) {
+
+}
+
+func InitDomainRegionTree(d Domain) *RegionTree {
+
+}
+
+func (RT *RegionTree) defaultRegion() {
+
+}
+
+func (RT *RegionTree) SearRegion(d Domain, s, e uint32) {
+
+}
+
+func (RT *RegionTree) AddRegion(d *Domain, r *Region) {
+
+}
+
+func (RT *RegionTree) UpdateRegion(d *Domain, r *Region) {
+
+}
+
+func (RT *RegionTree) DelRegion(d *Domain, r *Region) {
+
+}
+
+func NewDomain(d string, ns []*dns.NS, t uint32) (*Domain, *MyError.MyError) {
+	if _, ok := dns.IsDomainName(d); !ok {
+		return MyError.NewError(MyError.ERROR_PARAM, d+" is not valid domain name")
+	}
+	//	if ns != nil {
+	//		for _,n := range ns {
+	//			fmt.Println(n)
+	//		}
+	//	}
+	return &Domain{
+		DomainName: dns.Fqdn(d),
+		NS:         ns,
+		TTL:        t,
+	}
 }
 
 //@TODO: randow cf.Servers for load banlance
@@ -72,18 +155,6 @@ func GetDomainResolver(domainName string) (string, string, *MyError.MyError) {
 		//		return "", "", e
 	}
 	return domainResolverIP, domainResolverPort, nil
-}
-
-func init() {
-	domainDB := InitDomainDB()
-	bd_ns := []string{"ns1.baidu.com", "ns2.baidu.com"}
-	dt, _ := NewDomain("www.baidu.com", bd_ns, 86400)
-	domainDB.Insert(dt)
-	sina_ns := []string{"ns1.sina.com", "ns2.sina.com"}
-	dt, _ = NewDomain("api.weibo.cn", sina_ns, 86400)
-	domainDB.Insert(dt)
-	dt, _ = NewDomain("api.weibo.cn", sina_ns, 86400)
-	domainDB.Insert(dt)
 }
 
 func GetDomainConfigFromDomainTree(domain string) (string, string, *MyError.MyError) {
@@ -114,35 +185,5 @@ func GetDomainConfigFromDomainTree(domain string) (string, string, *MyError.MyEr
 			ds = "8.8.8.8"
 		}
 	}
-
 	return ds, dp, nil
-}
-
-func (d *DomainConfig) SetTtl(t string) error {
-	if ti, e := strconv.Atoi(t); e == nil {
-		if ti > 0 && ti < 65535 {
-			d.Ttl = t
-		} else {
-			return errors.New(t + "is not permited")
-		}
-	}
-	return nil
-}
-
-func (d *DomainConfig) SetAS(as []string) error {
-	if len(d.AuthoritativeServers) != 0 {
-		d.AuthoritativeServers = nil
-	}
-	for _, s := range as {
-		if ip := net.ParseIP(s); ip != nil {
-			d.AuthoritativeServers = append(d.AuthoritativeServers, s)
-		} else {
-			return errors.New(s + " is not standard ip string")
-		}
-	}
-	return nil
-}
-
-func CompareDomain(a, b *DomainConfig) int {
-	return strings.Compare(a.DomainName, b.DomainName)
 }
