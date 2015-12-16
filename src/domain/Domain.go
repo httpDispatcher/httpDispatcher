568646fb49afd7b1c98be9d677840521ed73aaf3
WangChunsheng <chunshengster@gmail.com>
2015-12-16 16:39:57 +0800

    Add many functions ......

--- src/domain/Domain.go
+++ src/domain/Domain.go
@@ -4,62 +4,89 @@ import (
 	"MyError"
 	"fmt"
 	"os"
+	"query"
+	"reflect"
 	"sync"
 	"time"
 
+	"github.com/miekg/bitradix"
 	"github.com/miekg/dns"
 	"github.com/petar/GoLLRB/llrb"
 )
+
+const radix_bit = 5
 
 type MuLLRB struct {
 	llrb.LLRB
 	sync.Mutex
 }
 
+type MubitRadix struct {
+	bitradix.Radix32
+	sync.Mutex
+}
+
 //TODO: redundant data types, need to be redesign
 type Domain struct {
 	DomainName string
-	NS         []*dns.NS
+	SOA        string
 	TTL        uint32
 }
 
 type DomainNode struct {
 	Domain
-	RegionRRTree *MuLLRB
+	DomainRegionTree *RegionTree
 }
 
 //TODO: redundant data types, need to be redesign
 // dns.RR && RrType && TTL
 type Region struct {
-	IpStart    uint32
-	IpEnd      uint32
-	RR         []*dns.RR
-	RrType     dns.Type
-	TTL        uint32
-	UpdateTime time.Time
+	NetWorkAddr uint32
+	IpStart     uint32
+	IpEnd       uint32
+	RR          []dns.RR
+	RrType      uint16
+	TTL         uint32
+	UpdateTime  time.Time
+}
+
+type DomainSOANode struct {
+	DomainName string
+	NS         []*dns.NS
+	SOA        *dns.SOA
 }
 
 type DomainConfig struct {
-	DomainName           string   `domainName`
-	AuthoritativeServers []string `asServers`
+	DomainName           string
+	AuthoritativeServers []string
 	Port                 string
 	Ttl                  string
 }
 
-type DomainTree MuLLRB
-type RegionTree MuLLRB
+//For domain name and Region RR
+type DomainRRTree MuLLRB
+
+//For domain SOA and NS record
+type DomainSOATree MuLLRB
+
+//For domain Region and A/CNAME record
+type RegionTree MubitRadix
 
 var once sync.Once
-var DomainDB *DomainTree
+var DomainRRDB *DomainRRTree
+var DomainSOADB *DomainSOATree
 
 func init() {
-	db := InitDomainDB()
-	if db == nil {
-		fmt.Println("InitDomainDB failed!")
-		os.Exit(2)
+	errdb := InitDB()
+
+	if errdb == nil {
+		fmt.Println("InitDomainRRDB OK")
+		fmt.Println("InitDomainSOADB OK")
 	} else {
-		fmt.Println("InitDomainDB succ!")
+		fmt.Println("InitDomainRRDB() or InitDomainSOADB() failed")
+		os.Exit(2)
 	}
+
 }
 
 func (a *DomainNode) Less(b llrb.Item) bool {
@@ -68,7 +95,7 @@ func (a *DomainNode) Less(b llrb.Item) bool {
 	} else if y, ok := b.(*Domain); ok {
 		return a.DomainName < y.DomainName
 	}
-	return false
+	panic(MyError.NewError(MyError.ERROR_PARAM, "Param error of b: "+reflect.ValueOf(b).String()))
 }
 func (a *Domain) Less(b llrb.Item) bool {
 	if x, ok := b.(*DomainNode); ok {
@@ -76,42 +103,199 @@ func (a *Domain) Less(b llrb.Item) bool {
 	} else if y, ok := b.(*Domain); ok {
 		return a.DomainName < y.DomainName
 	}
-	return false
+	panic(MyError.NewError(MyError.ERROR_PARAM, "Param error of b: "+reflect.ValueOf(b).String()))
 }
 
-func InitDomainDB() *MuLLRB {
+func InitDB() *MyError.MyError {
 	once.Do(func() {
-		DomainDB = &MuLLRB{}
-		return DomainDB
+		DomainRRDB = &DomainRRTree{}
+		DomainSOADB = &DomainSOATree{}
 	})
 	return nil
 }
 
-func (DT *DomainTree) StoreDomain(d *Domain) {
+//func InitDomainSOADB() *MyError.MyError {
+//	once.Do(func() {
+//		DomainSOADB = &DomainSOATree{}
+//	})
+//	return nil
+//}
 
+// 1,Trust d.DomainName is really a DomainName, so, don't use dns.IsDomainName for checking
+// Check if d is already in the DomainRRTree,if so,make sure update d.DomainRegionTree = dt.DomainRegionTree
+func (DT *DomainRRTree) StoreDomainNode(d *DomainNode) (bool, *MyError.MyError) {
+	if dt, err := DT.SearchDomainNodeWithName(d.DomainName); dt != nil && err == nil {
+		fmt.Println("DomainRRTree already has DomainNode of d " + reflect.ValueOf(dt).String())
+		d.DomainRegionTree = dt.DomainRegionTree
+	}
+	DT.Mutex.Lock()
+	DT.LLRB.ReplaceOrInsert(d)
+	DT.Mutex.Unlock()
+	return true, nil
 }
 
-func (DT *DomainTree) SearchDomain(d *Domain) {
-
+func (DT *DomainRRTree) SearchDomainNodeWithName(d string) (*DomainNode, *MyError.MyError) {
+	if _, ok := dns.IsDomainName(d); ok {
+		dn := &Domain{
+			DomainName: dns.Fqdn(d),
+		}
+		return DT.SearchDomainNode(dn)
+	}
+	return nil, MyError.NewError(MyError.ERROR_PARAM, "Eorror param: "+reflect.ValueOf(d).String())
 }
 
-func (DT *DomainTree) UpdateDomain(d *Domain) {
-
+func (DT *DomainRRTree) SearchDomainNode(d *Domain) (*DomainNode, *MyError.MyError) {
+	dr := DT.LLRB.Get(d)
+	if dr != nil {
+		if drr, ok := dr.(*DomainNode); ok {
+			return drr, nil
+		} else {
+			return nil, MyError.NewError(MyError.ERROR_TYPE, "Got error result because of the type of return value is "+reflect.TypeOf(dr).String())
+		}
+	} else {
+		return nil, MyError.NewError(MyError.ERROR_NORESULT, "Got no result for param: "+reflect.ValueOf(d).String())
+	}
+	return nil, MyError.NewError(MyError.ERROR_UNKNOWN, "SearchDomainNode got param: "+reflect.ValueOf(d).String())
 }
 
-func InitDomainRegionTree(d Domain) *RegionTree {
-
+func (DT *DomainRRTree) UpdateDomainNode(d *DomainNode) (bool, *MyError.MyError) {
+	if _, ok := query.Check_DomainName(d.DomainName); ok {
+		if dt, err := DT.SearchDomainNodeWithName(d.DomainName); dt != nil && err == nil {
+			d.DomainRegionTree = dt.DomainRegionTree
+			DT.Mutex.Lock()
+			DT.LLRB.ReplaceOrInsert(d)
+			DT.Mutex.Unlock()
+			return true, nil
+		} else {
+			return false, MyError.NewError(MyError.ERROR_NOTFOUND, "DomainRRTree does not has "+reflect.ValueOf(d).String()+" or it has "+reflect.ValueOf(dt).String())
+		}
+	} else {
+		return false, MyError.NewError(MyError.ERROR_PARAM, " Param d "+reflect.ValueOf(d).String()+" is not valid Domain instance")
+	}
+	return false, MyError.NewError(MyError.ERROR_UNKNOWN, "UpdateDomainNode return unknown error")
 }
 
-func (RT *RegionTree) defaultRegion() {
+//Use interface{} as param ,  may refact other func as this
+//TODO: this func has not been completed,don't use it
+func (DT *DomainRRTree) DelDomainNode(d interface{}) (bool, *MyError.MyError) {
+	var e *MyError.MyError
+	var dd *DomainNode
+	t := reflect.TypeOf(d).Kind()
+	fmt.Println(t)
+	switch t {
+	case reflect.String:
+		if ds, ok := d.(string); ok {
+			if _, ok := query.Check_DomainName(ds); ok {
 
+				dd, e = NewDomainNode(ds, "", 0)
+			}
+		} else {
+			return false, MyError.NewError(MyError.ERROR_PARAM, "Error in type of param "+reflect.ValueOf(d).String())
+		}
+	case reflect.TypeOf(&Domain{}).Kind():
+	case reflect.TypeOf(&DomainNode{}).Kind():
+	default:
+		fmt.Println("fdjlsjflsjdlfj")
+	}
+
+	if e == nil {
+		DT.Mutex.Lock()
+		r := DT.LLRB.Delete(dd)
+		DT.Mutex.Unlock()
+		if r != nil {
+			return true, nil
+		} else {
+			return false, nil
+		}
+	}
+	return false, nil
 }
 
-func (RT *RegionTree) SearRegion(d Domain, s, e uint32) {
-
+func InitDomainSOANode(d string,
+	soa *dns.SOA,
+	ns_a []*dns.NS) *DomainSOANode {
+	return &DomainSOANode{
+		DomainName: d,
+		NS:         ns_a,
+		SOA:        soa,
+	}
 }
 
-func (RT *RegionTree) AddRegion(d *Domain, r *Region) {
+func (a *DomainSOANode) Less(b llrb.Item) bool {
+	if x, ok := b.(*DomainSOANode); ok {
+		return a.DomainName < x.DomainName
+	}
+	panic(MyError.NewError(MyError.ERROR_PARAM, "Param b "+reflect.ValueOf(b).String()+" is not valid DomainSOANode or String"))
+}
+
+func (ST *DomainSOATree) StoreDomainSOANode(dsn *DomainSOANode) (bool, *MyError.MyError) {
+	ST.Mutex.Lock()
+	ST.LLRB.ReplaceOrInsert(dsn)
+	ST.Mutex.Unlock()
+	return true, nil
+}
+
+func (ST *DomainSOATree) GetDomainSOANode(dsn *DomainSOANode) (*DomainSOANode, *MyError.MyError) {
+	dt := ST.LLRB.Get(dsn)
+	if dsn_r, ok := dt.(*DomainSOANode); ok {
+		return dsn_r, nil
+	} else {
+		return nil, MyError.NewError(MyError.ERROR_TYPE, "ERROR_TYPE")
+	}
+}
+
+func (ST *DomainSOATree) SearchDomainSOANodeWithDomainName(d string) (*DomainSOANode, *MyError.MyError) {
+	ds := &DomainSOANode{
+		DomainName: dns.Fqdn(d),
+	}
+	fmt.Println(ds)
+	fmt.Println(ST)
+	//	dsn := ST.LLRB.Get(ds)
+	//	if dsn != nil{
+	//		if dsn_r,ok := dsn.(*DomainSOANode); ok{
+	//			return dsn_r, nil
+	//		}else{
+	//			fmt.Println(dsn)
+	//		}
+	//		return nil,MyError.NewError(MyError.ERROR_UNKNOWN,"DomainSOATree returned Not DomainSOANode Type OBJ,the value of returned is " + reflect.ValueOf(dsn).String())
+	//	}
+	return nil, MyError.NewError(MyError.ERROR_NORESULT, "Returned no result")
+}
+
+func (ST *DomainSOATree) UpdateDomainSOANode(ds *DomainSOANode) *MyError.MyError {
+	ST.LLRB.ReplaceOrInsert(ds)
+	return nil
+}
+
+func (ST *DomainSOATree) DelDomainSOANode(ds *DomainSOANode) *MyError.MyError {
+	ST.LLRB.Delete(ds)
+	return nil
+}
+
+func (a *DomainNode) InitRegionTree() (bool, *MyError.MyError) {
+	if a.DomainRegionTree == nil {
+		a.DomainRegionTree = initDomainRegionTree()
+	}
+	return true, nil
+}
+
+func initDomainRegionTree() *RegionTree {
+	//	tbitRadix := bitradix.New32()
+	return &RegionTree{}
+}
+
+func (RT *RegionTree) SearchRegion(addr uint32) {
+	r := RT.Radix32.Find(addr, radix_bit)
+	fmt.Println(r)
+	RT.Radix32.Do(func(r1 *bitradix.Radix32, i int) {
+		fmt.Println(r1.Key(),
+			r1.Value,
+			r1.Bits(),
+			r1.Leaf(), i)
+	})
+}
+
+func (RT *RegionTree) AddRegion(r *Region) {
 
 }
 
@@ -123,67 +307,41 @@ func (RT *RegionTree) DelRegion(d *Domain, r *Region) {
 
 }
 
-func NewDomain(d string, ns []*dns.NS, t uint32) (*Domain, *MyError.MyError) {
+func NewRegion(r []dns.RR, networkAddr, ipStart, ipEnd uint32) (*Region, *MyError.MyError) {
+	if cap(r) < 1 {
+		return nil, MyError.NewError(MyError.ERROR_PARAM, "cap of r ([]dns.RR) can not be less then 1 ")
+	}
+	// When the default region for a domain, the networkAddr and ipStart/ipEnd will be 0
+	//	if (networkAddr == 0) || (ipStart == 0) || (ipEnd == 0) {
+	//		return nil, MyError.NewError(MyError.ERROR_PARAM, "networkAddr, ipStart, ipEnd could not be 0")
+	//	}
+	dr := &Region{
+		NetWorkAddr: networkAddr,
+		IpStart:     ipStart,
+		IpEnd:       ipEnd,
+		RR:          r,
+		RrType:      r[0].Header().Rrtype,
+		TTL:         r[0].Header().Ttl,
+		UpdateTime:  time.Now(),
+	}
+	return dr, nil
+}
+
+func NewDomainNode(d string, soa string, t uint32) (*DomainNode, *MyError.MyError) {
 	if _, ok := dns.IsDomainName(d); !ok {
-		return MyError.NewError(MyError.ERROR_PARAM, d+" is not valid domain name")
+		return nil, MyError.NewError(MyError.ERROR_PARAM, d+" is not valid domain name")
 	}
 	//	if ns != nil {
 	//		for _,n := range ns {
 	//			fmt.Println(n)
 	//		}
 	//	}
-	return &Domain{
-		DomainName: dns.Fqdn(d),
-		NS:         ns,
-		TTL:        t,
-	}
-}
-
-//@TODO: randow cf.Servers for load banlance
-func GetDomainResolver(domainName string) (string, string, *MyError.MyError) {
-	if _, ok := dns.IsDomainName(domainName); !ok {
-		return "", "", MyError.NewError(MyError.ERROR_PARAM, "Parma is not a domain name : "+domainName)
-	}
-	domainResolverIP, domainResolverPort, e := GetDomainConfigFromDomainTree(domainName)
-	if (e != nil) && (e.ErrorNo == MyError.ERROR_NORESULT) {
-		//TODO: 后台获取 domainName 的 Resolver 信息,并存储
-		//QueryNS()
-		domainResolverIP = "114.114.114.114"
-		domainResolverPort = "53"
-
-	} else {
-		//		return "", "", e
-	}
-	return domainResolverIP, domainResolverPort, nil
-}
-
-func GetDomainConfigFromDomainTree(domain string) (string, string, *MyError.MyError) {
-	var ds, dp string
-	dp = "53"
-	domain = dns.Fqdn(domain)
-	switch domain {
-	case "www.baidu.com.":
-		ds = "ns2.baidu.com."
-	case "www.a.shifen.com.":
-		ds = "ns1.a.shifen.com."
-	case "a.shifen.com.":
-		ds = "ns1.a.shifen.com."
-	case "ww2.sinaimg.cn.":
-		ds = "ns1.sina.com.cn."
-	case "weiboimg.gslb.sinaedge.com.":
-		ds = "ns2.sinaedge.com."
-	case "weiboimg.grid.sinaedge.com.":
-		ds = "ns1.sinaedge.com."
-	case "api.weibo.cn.":
-		ds = "ns1.sina.com.cn."
-	case "img.alicdn.com.":
-		ds = "ns8.alibabaonline.com."
-	case "img.alicdn.com.danuoyi.alicdn.com.":
-		ds = "danuoyinewns1.gds.alicdn.com."
-	default:
-		if _, ok := dns.IsDomainName(domain); ok {
-			ds = "8.8.8.8"
-		}
-	}
-	return ds, dp, nil
+	return &DomainNode{
+		Domain: Domain{
+			DomainName: dns.Fqdn(d),
+			SOA:        soa,
+			TTL:        t,
+		},
+		DomainRegionTree: nil,
+	}, nil
 }
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"MyError"
+	"encoding/binary"
+	"fmt"
 	"log"
+	"net"
 	"os"
 )
 
@@ -25,6 +29,69 @@ func InitUitls() {
 	Logger.Println("Starting httpDispacher...")
 }
 
-func UnpackIPCIDR(i string) {
+//func IP4toInt(IPv4Address net.IP) int64 {
+//	IPv4Int := big.NewInt(0)
+//
+//	IPv4Int.SetBytes(IPv4Address.To4())
+//	fmt.Println(IPv4Int.Bytes())
+//	fmt.Printf("%v", IPv4Int.Bytes())
+//	return IPv4Int.Int64()
+//}
 
+func NetworkRange(network *net.IPNet) (net.IP, net.IP) {
+	fmt.Println(network)
+	//	os.Exit(2)
+	netIP := network.IP.To4()
+	firstIP := netIP.Mask(network.Mask)
+	lastIP := net.IPv4(0, 0, 0, 0).To4()
+	for i := 0; i < len(lastIP); i++ {
+		lastIP[i] = netIP[i] | ^network.Mask[i]
+	}
+	return firstIP, lastIP
+}
+
+// Converts a 4 bytes IP into a 32 bit integer
+func Ip4ToInt32(ip net.IP) int32 {
+	return int32(binary.BigEndian.Uint32(ip.To4()))
+}
+
+// Converts 32 bit integer into a 4 bytes IP address
+func Int32ToIP4(n int32) net.IP {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(n))
+	return net.IP(b)
+}
+
+// Bigger than we need, not too big to worry about overflow
+const big = 0xFFFFFF
+
+func dtoi(s string, i0 int) (n int, i int, ok bool) {
+	n = 0
+	for i = i0; i < len(s) && '0' <= s[i] && s[i] <= '9'; i++ {
+		n = n*10 + int(s[i]-'0')
+		if n >= big {
+			return 0, i, false
+		}
+	}
+	if i == i0 {
+		return 0, i, false
+	}
+	return n, i, true
+}
+
+func ParseEdnsIPNet(ip net.IP, mask uint8, family uint16) (*net.IPNet, *MyError.MyError) {
+	fmt.Println(mask)
+	iplen := 0
+	switch family {
+	case 1:
+		iplen = net.IPv4len
+	case 2:
+		iplen = net.IPv6len
+	}
+	n, i, ok := dtoi(string(mask), 0)
+	if ip == nil || !ok || i != len(string(mask)) || n < 0 || n > 8*iplen {
+		return nil, MyError.NewError(MyError.ERROR_NOTVALID, "ParseEdnsIPNet error, param: "+ip.String()+"/"+string(mask))
+	}
+	m := net.CIDRMask(n, 8*iplen)
+	return &net.IPNet{IP: ip.Mask(m), Mask: m}, nil
 }
