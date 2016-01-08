e1102545e9935e2bb3e157aadc9c5ed1d0e11c5b
WangChunsheng <chunshengster@gmail.com>
2016-01-08 16:43:29 +0800

    change with mysql schema changed .

--- src/query/query_mysql_test.go
+++ src/query/query_mysql_test.go
@@ -1,6 +1,7 @@
 package query
 
 import (
+	"MyError"
 	"github.com/miekg/dns"
 	"testing"
 )
@@ -15,9 +16,18 @@ func TestInitMySQL(t *testing.T) {
 }
 
 func TestGetDomainIDFromMySQL(t *testing.T) {
-	d_a := []string{"www.sina.com.cn", "www.baidu.com", "www.a.shifen.com", "api.weibo.cn", "weibo.cn", "sinaedge.com"}
+	d_a := []string{
+		"www.sina.com.cn",
+		"www.baidu.com",
+		"www.a.shifen.com",
+		"api.weibo.cn",
+		"weibo.cn",
+		"sinaedge.com",
+		"ww2.sinaimg.cn",
+	}
 
 	for _, d := range d_a {
+		t.Log(d)
 		id, e := RRMySQL.GetDomainIDFromMySQL(d)
 		if e != nil {
 			t.Log(id)
@@ -30,35 +40,40 @@ func TestGetDomainIDFromMySQL(t *testing.T) {
 	}
 }
 
-func TestGetDomainRegionWithIPFromMySQL(t *testing.T) {
-	d_a := []string{"www.sina.com.cn", "www.baidu.com", "www.a.shifen.com", "api.weibo.cn", "weibo.cn", "sinaedge.com"}
-	ipuint32 := uint32(10245)
-	for _, d := range d_a {
-		id, e := RRMySQL.GetDomainRegionWithIPFromMySQL(d, ipuint32)
-		if e == nil {
-			t.Log(id.idRegionID, id.Region)
-		} else {
-			t.Log(e)
-		}
+func TestGetRegionWithIPFromMySQL(t *testing.T) {
+	// d_a := []string{"www.sina.com.cn", "www.baidu.com", "www.a.shifen.com", "api.weibo.cn", "weibo.cn", "sinaedge.com"}
+	ipuint32 := uint32(1790519448)
+	id, e := RRMySQL.GetRegionWithIPFromMySQL(ipuint32)
+	if e == nil {
+		t.Log(id.Region)
+	} else {
+		t.Log(e)
 	}
 }
 
 func TestGetRRFromMySQL(t *testing.T) {
+	d_a := []uint32{1, 2}
 	r_a := []uint32{1, 2, 3, 4, 5}
-	for _, id := range r_a {
-		x, e := RRMySQL.GetRRFromMySQL(id)
-		if e == nil {
-			for _, xx := range x {
-				t.Log(xx, xx.RR)
-				if xx.RR.RrType == dns.TypeA {
-					t.Log("A RR")
-				} else if xx.RR.RrType == dns.TypeCNAME {
-					t.Log("CNAME RR")
+	for _, d := range d_a {
+		for _, id := range r_a {
+			x, e := RRMySQL.GetRRFromMySQL(d, id)
+			if e == nil {
+				t.Log("DomainId: ", d, " RegionId: ", id)
+				for _, xx := range x {
+					t.Log(xx, xx.RR)
+					if xx.RR.RrType == dns.TypeA {
+						t.Log("A RR")
+					} else if xx.RR.RrType == dns.TypeCNAME {
+						t.Log("CNAME RR")
+					}
+				}
+			} else {
+				if e.ErrorNo == MyError.ERROR_NORESULT {
+					t.Log(e)
+				} else {
+					t.Fail()
 				}
 			}
-		} else {
-			t.Log(e)
-			t.Fail()
 		}
 	}
 }
