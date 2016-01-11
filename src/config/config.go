a008739c4ab8890f3e09feeabdc2fc546d85085e
WangChunsheng <chunshengster@gmail.com>
2016-01-11 17:01:26 +0800

    Add MySQL schema

--- src/config/config.go
+++ src/config/config.go
@@ -3,14 +3,15 @@ package config
 import "github.com/miekg/dns"
 
 var List []string = []string{
-	"www.taobao.com",
-	"www.baidu.com",
-	"www.qq.com",
-	"www.meituan.com",
-	"www.sina.com.cn",
+	//	"www.taobao.com",
+	//	"www.baidu.com",
+	//	"www.qq.com",
+	//	"www.meituan.com",
+	//	"www.sina.com.cn",
 	"api.weibo.cn",
 	"weibo.cn",
-	"ww2.sinaimg.cn"}
+	//	"ww2.sinaimg.cn",
+}
 
 func IsLocalMysqlBackend(d string) bool {
 	for _, x := range List {
