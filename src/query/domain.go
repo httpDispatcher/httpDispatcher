17c821dc0f5a8fb46ccb8467d37d6b646849d987
chunsheng <chunshengster@gmail.com>
2017-12-07 11:53:49 +0800

    refact domain.go , move global variable *DomainRRTree and *DomainSOATree to query/init.go

--- src/query/domain.go
+++ src/query/domain.go
@@ -1,7 +1,6 @@
 package query
 
 import (
-	"os"
 	"reflect"
 	"sync"
 	"time"
@@ -144,38 +143,7 @@ type DomainConfig struct {
 
 var once sync.Once
 
-//DomainRRCache for Domain A/CNAME record
-var DomainRRCache *DomainRRTree
 
-//DomainSOACache for Domain soa/cname record
-var DomainSOACache *DomainSOATree
-
-//if you want to search a A/CNAME record for a domain 'domainname',you should:
-//First: search a DomainNode in domainRRCache and get domain region tree
-// with DomainNode.RegionTree, and than get the record in the region tree
-//Second: if there is no DomainNode in DomainRRCache, you should get DomainSOANode in
-// DomainSOACache and get NS with DomainSOANode.NS. Notice that ,DomainSOANode.NS is
-// a slice of *dns.NS.
-//Third: Use query
-// .QueryA with the one name server in DomainSOANode.NS.
-//Notice: you should store all the infoformation when it is not in the trees(
-// Both DomainSOACache and DomainRRCache )
-
-func init() {
-	errCache := InitCache()
-
-	if errCache == nil {
-		utils.ServerLogger.Critical(utils.GetDebugLine(), "InitDomainRRCache OK")
-		utils.ServerLogger.Critical(utils.GetDebugLine(), "InitDomainSOACache OK")
-	} else {
-		//fmt.Println(utils.GetDebugLine(), "InitDomainRRCache() or InitDomainSOACache() failed")
-		//fmt.Println(utils.GetDebugLine(), "Plase contact chunshengster@gmail.com to get more help ")
-		utils.ServerLogger.Critical("InitDomainRRCache() or InitDomainSOACache() failed")
-		utils.ServerLogger.Info("Plase contact chunshengster@gmail.com to get more help")
-		os.Exit(2)
-	}
-
-}
 
 func InitCache() *MyError.MyError {
 	once.Do(func() {
